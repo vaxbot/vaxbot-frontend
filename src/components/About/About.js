4bf6187f4f7ba4a92931bb11be8130e37efa3c81
austinc <ac6404@gmail.com>
2021-03-11 16:14:02 -0600

    update about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,40 @@ import "./About.css";
 function About() {
   return (
     <div id="about">
-      <p>I'm baby ennui gastropub beard lyft, man bun cronut edison bulb flexitarian. Gastropub church-key keffiyeh pop-up man braid. Typewriter gentrify +1 tumblr vexillologist hammock hot chicken humblebrag quinoa twee. Ramps knausgaard subway tile offal hexagon, vexillologist chia williamsburg put a bird on it scenester retro. Cronut cliche keffiyeh, roof party mlkshk chia sartorial tote bag bicycle rights artisan. Ethical kitsch letterpress neutra blue bottle, health goth williamsburg.</p>
-      <p>Jean shorts stumptown plaid hella next level meh polaroid gastropub cronut. Coloring book green juice kogi, cred fashion axe tousled 8-bit letterpress YOLO. Swag readymade synth godard ramps affogato fashion axe williamsburg food truck stumptown biodiesel man braid hell of everyday carry. Four loko tousled banjo master cleanse palo santo pabst. Jean shorts chartreuse raw denim, gentrify literally kitsch quinoa microdosing pinterest occupy four loko.</p>
-      <p>Ethical ramps chartreuse salvia, sriracha subway tile kitsch bitters. Literally green juice activated charcoal, roof party iceland chicharrones meditation lyft street art blog. Prism lumbersexual trust fund, jianbing tilde migas leggings ethical organic tousled health goth pop-up iPhone PBR&B. Post-ironic stumptown polaroid man bun actually kitsch. Coloring book yr YOLO hot chicken kale chips, meditation tumeric. Twee 8-bit selfies chia, hammock wolf pickled mixtape. Lo-fi pabst fingerstache, adaptogen cray cloud bread flannel DIY palo santo narwhal blog.</p>
+<h3>So, What is vaxbot?</h3>
+
+<p>Vaxbot is designed to help people save time searching for COVID-19 vaccines. Instead of having to check multiple sites and fill out eligibility forms
+on each, vaxbot can confirm vaccine availability BEFORE you go through all that.</p> 
+
+<h3>Who built this and why?</h3>
+
+<p>Vaxbot was built by Austin Carnahan. The original idea was inspired by Huge Ma, the creator of the NYC-based turbovax.com. We've borrowed a lot of ideas
+and design from them, huge thanks! We've also learned a lot from Nick Something's vaccinespotter.com and their code has helped us vastly improve our pharmacy searches.</p>
+
+
+<p>Recently, Missouri has consistly ranked last in measures of percent of population vaccinated. We wanted to bring some of the most forward-thinking tools
+to our community and help facilitate the proccess of getting more people vaccinated.</p>
+
+<h3>What makes vabot unique?</h3>
+
+<p>While there are new tools being published every day, we think vaxbot is special in that it automates collection of data at a national level AND has a strong focus on
+regional community engagement. We find dedicated volunteers to run regional social media channels which are informed by our national scans and who help us include 
+more local resources.</p>
+
+<h3>When I follow your links, I'm still not seeing available vaccine appointments</h3>
+
+<p>We try to update our data as frequently as possible without disrupting the services that provide our information. 
+Vaccine appointments fill up very fast. Refresh the site and check back often for the best chance at securing an appointment.</p>
+
+
+<h3>Am I eligible for a vaccine?</h3>
+
+<p>We don't keep track of state or federal eligibility requirements. Make sure you qualify for vaccines in your area.</p> 
+
+<h3>How can I contribute?</h3>
+
+<p>To donate: please follow the Buy Me A Coffe link on our homepage.
+If you'd like to collaborate or contribute otherwise, please check our Github repositories or email us at vaxbotstl@gmail.com</p>
     </div>
   )
 }
